bbd924b5473c901eea59069d292833b8308bebab
HumbertoSanchas <humbertosanchas@hotmail.com>
2016-04-03 15:17:03 -0400

    ass5

---
+++ FormTemplate/FormTemplate/MyForm.cpp
@@ -0,0 +1,12 @@
+#include "MyForm.h"
+using namespace System;
+using namespace System::Windows::Forms;
+using namespace FormTemplate;
+
+[STAThreadAttribute]
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew MyForm());
+}
